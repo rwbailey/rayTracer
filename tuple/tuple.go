c678e7b5b8be388ca17ec8bd98ec10d008f480f2
Richard Bailey <richard@richardbailey.xyz>
2021-02-08 12:54:18 +0000

    Add some comments

--- tuple/tuple.go
+++ tuple/tuple.go
@@ -11,37 +11,37 @@ type Tuple struct {
 	W float64
 }
 
+// New returns a new tuple
 func New(x, y, z, w float64) Tuple {
 	return Tuple{x, y, z, w}
 }
 
+// Point returns a new point
 func Point(x, y, z float64) Tuple {
 	return New(x, y, z, 1.0)
 }
 
+// Vector returns new vector
 func Vector(x, y, z float64) Tuple {
 	return New(x, y, z, 0.0)
 }
 
+// IsPoint returns true if t is a point, else returns false
 func (t Tuple) IsPoint() bool {
 	return t.W == 1.0
 }
 
+// IsVector returns true if t is a vector, else returns false
 func (t Tuple) IsVector() bool {
 	return t.W == 0.0
 }
 
+// Equals returns true if t1 == t2, else returns false
 func (t1 Tuple) Equals(t2 Tuple) bool {
 	return equals(t1.X, t2.X) && equals(t1.Y, t2.Y) && equals(t1.Z, t2.Z) && equals(t1.W, t2.W)
 }
 
-func equals(a, b float64) bool {
-	if math.Abs(a-b) < epsilon {
-		return true
-	}
-	return false
-}
-
+// Add returns t1 + t2
 func (t1 Tuple) Add(t2 Tuple) Tuple {
 	return Tuple{
 		X: t1.X + t2.X,
@@ -51,6 +51,7 @@ func (t1 Tuple) Add(t2 Tuple) Tuple {
 	}
 }
 
+// Subtract returns t1 - t2
 func (t1 Tuple) Subtract(t2 Tuple) Tuple {
 	return Tuple{
 		X: t1.X - t2.X,
@@ -60,11 +61,20 @@ func (t1 Tuple) Subtract(t2 Tuple) Tuple {
 	}
 }
 
-func (t Tuple) Negate() Tuple {
+// Negate returns -t when given t
+func Negate(t Tuple) Tuple {
 	return Tuple{
 		X: t.X * -1,
 		Y: t.Y * -1,
 		Z: t.Z * -1,
 		W: t.W * -1,
 	}
+}
+
+// Compare the equivelance of two floating point numbers to within the error margin epsilon
+func equals(a, b float64) bool {
+	if math.Abs(a-b) < epsilon {
+		return true
+	}
+	return false
 }
