807417013997863e4f083431f09628558013b972
Richard Bailey <richard@richardbailey.xyz>
2021-02-12 22:30:15 +0000

    Colour methods

--- colour/colour.go
+++ colour/colour.go
@@ -1,5 +1,9 @@
 package colour
 
+import "math"
+
+const epsilon = 0.00001
+
 type colour struct {
 	Red   float64
 	Green float64
@@ -12,4 +16,49 @@ func New(r, g, b float64) colour {
 		Green: g,
 		Blue:  b,
 	}
+}
+
+func (c1 colour) Add(c2 colour) colour {
+	return colour{
+		Red:   c1.Red + c2.Red,
+		Green: c1.Green + c2.Green,
+		Blue:  c1.Blue + c2.Blue,
+	}
+}
+
+func (c1 colour) Subtract(c2 colour) colour {
+	return colour{
+		Red:   c1.Red - c2.Red,
+		Green: c1.Green - c2.Green,
+		Blue:  c1.Blue - c2.Blue,
+	}
+}
+
+func (c colour) Multiply(i float64) colour {
+	return colour{
+		Red:   c.Red * i,
+		Green: c.Green * i,
+		Blue:  c.Blue * i,
+	}
+}
+
+func (c1 colour) Product(c2 colour) colour {
+	return colour{
+		Red:   c1.Red * c2.Red,
+		Green: c1.Green * c2.Green,
+		Blue:  c1.Blue * c2.Blue,
+	}
+}
+
+// Equals returns true if t1 == t2, else returns false
+func (a colour) Equals(b colour) bool {
+	return floatEquals(a.Red, b.Red) && floatEquals(a.Green, b.Green) && floatEquals(a.Blue, b.Blue)
+}
+
+// Compare the equivelance of two floating point numbers to within the error margin epsilon
+func floatEquals(a, b float64) bool {
+	if math.Abs(a-b) < epsilon {
+		return true
+	}
+	return false
 }
--- colour/colour_test.go
+++ colour/colour_test.go
@@ -3,16 +3,53 @@ package colour_test
 import (
 	"testing"
 
-	"github.com/rwbailey/ray/colour"
+	. "github.com/rwbailey/ray/colour"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateColour(t *testing.T) {
+func TestColourCreation(t *testing.T) {
 	// Given
-	c := colour.New(-0.5, 0.4, 1.7)
+	c := New(-0.5, 0.4, 1.7)
 
 	// Then
 	assert.EqualValues(t, -0.5, c.Red)
 	assert.EqualValues(t, 0.4, c.Green)
 	assert.EqualValues(t, 1.7, c.Blue)
+}
+
+func TestColourAddition(t *testing.T) {
+	// Given
+	c1 := New(0.9, 0.6, 0.75)
+	c2 := New(0.7, 0.1, 0.25)
+
+	// Then
+	assert.EqualValues(t, New(1.6, 0.7, 1.0), c1.Add(c2))
+}
+
+func TestColourSubtraction(t *testing.T) {
+	// Given
+	c1 := New(0.9, 0.6, 0.75)
+	c2 := New(0.7, 0.1, 0.25)
+
+	c3 := New(0.2, 0.5, 0.5)
+
+	// Then
+	assert.True(t, c3.Equals(c1.Subtract(c2)))
+}
+
+func TestColourMultiplicationByScalar(t *testing.T) {
+	// Given
+	c := New(0.2, 0.3, 0.4)
+
+	// Then
+	assert.EqualValues(t, New(0.4, 0.6, 0.8), c.Multiply(2.0))
+}
+
+func TestColourMultiplicationByColour(t *testing.T) {
+	// Given
+	a := New(1.0, 0.2, 0.4)
+	b := New(0.9, 1, 0.1)
+
+	// Then
+	assert.True(t, New(0.9, 0.2, 0.04).Equals(a.Product(b)))
 }
