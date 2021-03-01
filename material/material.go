08ec6be79d6abf957aa2d60e388c5e6033bdfc2b
Richard Bailey <richard@richardbailey.xyz>
2021-03-01 22:20:44 +0000

    Reflection

--- material/material.go
+++ material/material.go
@@ -1,6 +1,13 @@
 package material
 
-import "github.com/rwbailey/ray/colour"
+import (
+	"fmt"
+	"math"
+
+	"github.com/rwbailey/ray/colour"
+	"github.com/rwbailey/ray/light"
+	"github.com/rwbailey/ray/tuple"
+)
 
 type Material struct {
 	Colour    colour.Colour
@@ -18,4 +25,49 @@ func New() *Material {
 		Specular:  0.9,
 		Shininess: 200.0,
 	}
+}
+
+func (m *Material) Lighting(light *light.PointLight, point, eyev, normalv tuple.Tuple) colour.Colour {
+
+	var ambient colour.Colour
+	var diffuse colour.Colour
+	var specular colour.Colour
+
+	// Combine the surface colour with the light's colour
+	effectiveColour := m.Colour.Product(light.Intensity)
+
+	// Find the direction of the light source
+	lightv := light.Position.Subtract(point).Normalise()
+
+	// Compute the ambiant contribution
+	ambient = effectiveColour.Multiply(m.Ambient)
+
+	// lightDotNormal represents the cosine of the angle between the light vector and the normal vector.
+	// A negative number means the light is on the other side of the surface.
+	lightDotNormal := lightv.Dot(normalv)
+	if lightDotNormal < 0 {
+		diffuse = colour.New(0, 0, 0)
+		specular = colour.New(0, 0, 0)
+	} else {
+		// Compute diffuse contribution
+		diffuse = effectiveColour.Multiply(m.Diffuse * lightDotNormal)
+
+		// reflectDoteye represents the cosine of the angle between the reflection vector and the eye
+		// vector.
+		// A negative number means the light reflects away from the eye.
+		reflectv := lightv.Negate().Reflect(normalv)
+		reflectDotEye := reflectv.Dot(eyev)
+		if reflectDotEye <= 0 {
+			specular = colour.New(0, 0, 0)
+		} else {
+			// Compute the specular contribution
+			factor := math.Pow(reflectDotEye, m.Shininess)
+			specular = light.Intensity.Multiply(m.Specular * factor)
+		}
+	}
+
+	fmt.Println(ambient)
+	fmt.Println(diffuse)
+	fmt.Println(specular)
+	return ambient.Add(diffuse).Add(specular)
 }
--- material/material_test.go
+++ material/material_test.go
@@ -1,12 +1,18 @@
 package material_test
 
 import (
+	"math"
 	"testing"
 
 	"github.com/rwbailey/ray/colour"
+	"github.com/rwbailey/ray/light"
 	"github.com/rwbailey/ray/material"
+	"github.com/rwbailey/ray/tuple"
 	"github.com/stretchr/testify/assert"
 )
+
+var background = material.New()
+var point = tuple.Point(0, 0, 0)
 
 func TestDefaultMaterial(t *testing.T) {
 	// Given
@@ -18,4 +24,69 @@ func TestDefaultMaterial(t *testing.T) {
 	assert.EqualValues(t, 0.9, m.Diffuse)
 	assert.EqualValues(t, 0.9, m.Specular)
 	assert.EqualValues(t, 200.0, m.Shininess)
+}
+
+func TestLightingWithTheEyeBetweenTheLightAndTheSurface(t *testing.T) {
+	// Given
+	eyev := tuple.Vector(0, 0, -1)
+	normalv := tuple.Vector(0, 0, -1)
+	light := light.NewPointLight(tuple.Point(0, 0, -10), colour.New(1, 1, 1))
+
+	// When
+	result := background.Lighting(light, point, eyev, normalv)
+
+	// Then
+	assert.EqualValues(t, colour.New(1.9, 1.9, 1.9), result)
+}
+
+func TestLightingWithTheEyeBetweenTheLightAndTheSurface45Offset(t *testing.T) {
+	// Given
+	eyev := tuple.Vector(0, math.Sqrt(2)/2, -math.Sqrt(2)/2)
+	normalv := tuple.Vector(0, 0, -1)
+	light := light.NewPointLight(tuple.Point(0, 0, -10), colour.New(1, 1, 1))
+
+	// When
+	result := background.Lighting(light, point, eyev, normalv)
+
+	// Then
+	assert.EqualValues(t, colour.New(1.0, 1.0, 1.0), result)
+}
+
+func TestLightingWithTheOppositeSurfaceAndLightAt45Offset(t *testing.T) {
+	// Given
+	eyev := tuple.Vector(0, 0, -1)
+	normalv := tuple.Vector(0, 0, -1)
+	light := light.NewPointLight(tuple.Point(0, 10, -10), colour.New(1, 1, 1))
+
+	// When
+	result := background.Lighting(light, point, eyev, normalv)
+
+	// Then
+	assert.True(t, colour.New(0.7364, 0.7364, 0.7364).Equals(result))
+}
+
+func TestLightingWithEyeInPathOfReflectionVector(t *testing.T) {
+	// Given
+	eyev := tuple.Vector(0, -math.Sqrt(2)/2, -math.Sqrt(2)/2)
+	normalv := tuple.Vector(0, 0, -1)
+	light := light.NewPointLight(tuple.Point(0, 10, -10), colour.New(1, 1, 1))
+
+	// When
+	result := background.Lighting(light, point, eyev, normalv)
+
+	// Then
+	assert.True(t, colour.New(1.6364, 1.6364, 1.6364).Equals(result))
+}
+
+func TestLightingWithLightBehindTheSurface(t *testing.T) {
+	// Given
+	eyev := tuple.Vector(0, 0, -1)
+	normalv := tuple.Vector(0, 0, -1)
+	light := light.NewPointLight(tuple.Point(0, 0, 10), colour.New(1, 1, 1))
+
+	// When
+	result := background.Lighting(light, point, eyev, normalv)
+
+	// Then
+	assert.EqualValues(t, colour.New(0.1, 0.1, 0.1), result)
 }
