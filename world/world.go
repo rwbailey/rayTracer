b82d234ad4fdfa1a1bafee5fed9a5bbca599f6aa
Richard Bailey <richard@richardbailey.xyz>
2021-03-02 22:09:51 +0000

    Default world

--- world/world.go
+++ world/world.go
@@ -1,18 +1,44 @@
 package world
 
 import (
+	"github.com/rwbailey/ray/colour"
 	"github.com/rwbailey/ray/light"
+	"github.com/rwbailey/ray/matrix"
 	"github.com/rwbailey/ray/shape"
+	"github.com/rwbailey/ray/tuple"
 )
 
 type World struct {
-	Objects []*shape.Shape
+	Objects []shape.Shape
 	Light   *light.PointLight
 }
 
 func New() *World {
 	return &World{
-		Objects: []*shape.Shape{},
+		Objects: []shape.Shape{},
 		Light:   nil,
 	}
+}
+
+func Default() *World {
+	ls := light.NewPointLight(tuple.Point(-10, 10, -10), colour.New(1, 1, 1))
+
+	s1 := shape.NewSphere()
+	s1.Material.Colour = colour.New(0.8, 1.0, 0.6)
+	s1.Material.Diffuse = 0.7
+	s1.Material.Specular = 0.2
+
+	s2 := shape.NewSphere()
+	s2.Transform = matrix.Scaling(0.5, 0.5, 0.5)
+
+	w := New()
+
+	w.Light = ls
+
+	w.Objects = []shape.Shape{
+		s1,
+		s2,
+	}
+
+	return w
 }
--- world/world_test.go
+++ world/world_test.go
@@ -3,12 +3,16 @@ package world_test
 import (
 	"testing"
 
+	"github.com/rwbailey/ray/colour"
 	"github.com/rwbailey/ray/light"
+	"github.com/rwbailey/ray/matrix"
+	"github.com/rwbailey/ray/shape"
+	"github.com/rwbailey/ray/tuple"
 	"github.com/rwbailey/ray/world"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreatingAWorld(t *testing.T) {
+func TestCreatingAnEmptyWorld(t *testing.T) {
 	// Given
 	w := world.New()
 
@@ -18,4 +22,25 @@ func TestCreatingAWorld(t *testing.T) {
 
 	assert.EqualValues(t, 0, len(objects))
 	assert.EqualValues(t, (*light.PointLight)(nil), lgt)
+}
+
+func TestCreateDefaultWorld(t *testing.T) {
+	// Given
+	ls := light.NewPointLight(tuple.Point(-10, 10, -10), colour.New(1, 1, 1))
+
+	s1 := shape.NewSphere()
+	s1.Material.Colour = colour.New(0.8, 1.0, 0.6)
+	s1.Material.Diffuse = 0.7
+	s1.Material.Specular = 0.2
+
+	s2 := shape.NewSphere()
+	s2.Transform = matrix.Scaling(0.5, 0.5, 0.5)
+
+	// When
+	w := world.Default()
+
+	// Then
+	assert.EqualValues(t, w.Light, ls)
+	assert.Contains(t, w.Objects, s1)
+	assert.Contains(t, w.Objects, s2)
 }
